f42eff7dc68a7acf56df2579f17fa56ecf84a685
Jake <jakeor45@gmail.com>
2025-03-18 08:22:51 -0700

    fixed link

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Hello</h1>
-      <h4><a href="github.com/jacob-oreilly">Github</a></h4>
+      <h4><a href="https://github.com/jacob-oreilly">Github</a></h4>
       <h4><a href="/resume_2025">Resume</a></h4>
       <div className='projects'>
         <h4><a href="/softsono">Softosono</a></h4>
